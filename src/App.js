f0de7f69314080d1b0c3c4b564a20f1c2f03dea6
Paula Moheb <poulamoheb12@gmail.com>
2024-05-17 02:52:56 +0300

    adding personal data component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './css/App.css';
+import { div } from 'react';
+import appStyle from './css/App.module.css';
 import Header from './Header';
 
 function App() {
   return (
-    <>
+    <div className={appStyle.body}>
     <Header/>
-    </>
+    </div>
   );
 }
 
